fa1d618325645ed4f36c3b88064c84f1b3ff4df8
Ed Perkins <ed@edperkins.co.uk>
2018-02-20 17:10:09 +0000

    git commit -m "view ratings"
    Co-authored-by: edpe <ed@edperkins.co.uk>
    Co-authored-by: gleoman <teddy_goold@hotmail.com>
    Co-authored-by: elliewem <elliewem@gmail.com>
    Co-authored-by: jenniferemshepherd <jennifer.elaine.mairead@gmail.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,12 @@ class ReviewsController < ApplicationController
     def create
       @restaurant = Restaurant.find(params[:restaurant_id])
       @review = @restaurant.reviews.create(review_params)
+      p review_params
       redirect_to restaurant_path(@restaurant)
     end
 
     private
-    
+
     def review_params
       params.require(:review).permit(:reviewer,:rating,:comment)
     end
