57c0e548e13eec7196675b43b0e21cc55903e3f8
Ed Perkins <ed@edperkins.co.uk>
2018-02-20 14:46:50 +0000

    git commit -m "adds review comments and rating - basics"
    Co-authored-by: edpe <ed@edperkins.co.uk>
    Co-authored-by: gleoman <teddy_goold@hotmail.com>
    Co-authored-by: elliewem <elliewem@gmail.com>
    Co-authored-by: jenniferemshepherd <jennifer.elaine.mairead@gmail.com>

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,14 @@
+class ReviewsController < ApplicationController
+    def create
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @review = @restaurant.reviews.create(review_params)
+      redirect_to restaurant_path(@restaurant)
+    end
+
+    private
+    
+    def review_params
+      params.require(:review).permit(:reviewer,:rating,:comment)
+    end
+
+end
